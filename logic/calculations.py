f378e022b9191cc271e2496fb79b02198a4b5cfd
Ayush Kumar <kumarayush9651@tutanota.com>
2025-09-09 10:20:47 +0530

    BUG FIX : Update calculations.py :iregular behaviour of avoid fine button.

--- logic/calculations.py
+++ logic/calculations.py
@@ -23,7 +23,7 @@ def avoid_fine(absent,total,leave_days):
     new_absent=absent+(leave_days*lectures_per_day)
     new_percentage=(100-((new_absent/total)*100))
     #lectures_to_be_present=(10*new_absent)-total -> this line calculated the lectures to be present to avoid fine which starts from 90% 
-    lectures_to_be_present = ((100*absent)/(100-fine_starts_from)) - total # This line solves the issue and provides the output based on the user defined values of the fine starting percentages.
+    lectures_to_be_present = ((100*new_absent)/(100-fine_starts_from)) - total # This line solves the issue and provides the output based on the user defined values of the fine starting percentages.
     return lectures_to_be_present
 #get desired attendance
 def get_attendance(absent,total,desired_attendance):
